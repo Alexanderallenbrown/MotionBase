d64103edb934da1d085b07087ea0fa099ab427a2
Alex <brownaa@lafayette.edu>
2016-02-08 10:00:34 -0500

    Updated Simulink and matlab

--- Simulations/hii.m
+++ Simulations/hii.m
@@ -1,8 +1,17 @@
-rawdata=xlsread('rawdata.csv');
+rawdata=xlsread('rawdata.xlsx');
+%time,timestamp,recordtime,lat,long,alt,speed,course,verticalAccuracy,horizontalAccuracy,locTimeStamp,accelerationX,accelerationY,accelerationZ,HeadingX,HeadingY,HeadingZ,TrueHeading,MagneticHeading,HeadingAccuracy,RotationX,RotationY,RotationZ,motionYaw,motionRoll,motionPitch,motionRotationRateX,motionRotationRateY,motionRotationRateZ,motionUserAccelerationX,motionUserAccelerationY,motionUserAccelerationZ,en0,pdp_ip0,DeviceOrientation,Event
 time=rawdata(:,2);
-ax=rawdata(:,19);
-ay=rawdata(:,20);
-az=rawdata(:,21);
+anglex=rawdata(:,19);
+angley=rawdata(:,20);
+anglez=rawdata(:,21);
+
+ax=rawdata(:,11);
+ay=rawdata(:,12);
+az=rawdata(:,13);
+
+signalax=[time,ax];
+signalay=[time,ay];
+signalaz=[time,az];
 
 L1=[];
 L2=[];
@@ -24,8 +33,9 @@ a52=[];
 a61=[];
 a62=[];
 
-for i=1:length(ax)
-   [length1,l1,length2,l2,length3,l3,length4,l4,length5,l5,length6,l6]=traj(0,0,1,ax(i),ay(i),az(i)); 
+for i=1:length(xdesired)
+ 
+   [length1,l1,length2,l2,length3,l3,length4,l4,length5,l5,length6,l6]=traj(xdesired(i),ydesired(i),1+zdesired(i),anglex(i)+axtilt(i),angley(i)+aytilt(i),anglez(i)); 
    L1=[L1,length1];
    L2=[L2,length2];
    L3=[L3,length3];
@@ -33,14 +43,14 @@ for i=1:length(ax)
    L5=[L5,length5];
    L6=[L6,length6];
    
-   [angle11,angle12]=legangle(l1,length1,1,1);
-   a11=[a11,angle11];
-   a12=[a12,angle12];
+%    [angle11,angle12]=legangle(l1,length1,1,1);
+%    a11=[a11,angle11];
+%    a12=[a12,angle12];
    
 end
 
 figure(1)
-plot(time,L1)
+plot(time(1:101),L1)
 
-figure(2)
-plot(time,az')
+
+
