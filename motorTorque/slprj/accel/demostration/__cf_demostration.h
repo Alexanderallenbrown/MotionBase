f7675a8583d068b338f49309a922433ce58a45a8
megantuttle <tuttlem21@gmail.com>
2016-03-04 20:08:01 -0500

    not much change

--- motorTorque/slprj/accel/demostration/__cf_demostration.h
+++
@@ -1,3 +0,0 @@
-#ifndef CF_demostration_H__
-#define CF_demostration_H__
-#endif
--- motorTorque/slprj/accel/demostration/demostration_acc_types.h
+++
@@ -1,7 +0,0 @@
-#include "__cf_demostration.h"
-#ifndef RTW_HEADER_demostration_acc_types_h_
-#define RTW_HEADER_demostration_acc_types_h_
-#include "rtwtypes.h"
-#include "multiword_types.h"
-typedef struct aa5zavtgxb_ aa5zavtgxb ;
-#endif
