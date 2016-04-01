46652f81bf6521b25c0208f1b7276b8cd1f07654
andy <wangze@lafayette.edu>
2016-04-01 15:32:29 -0400

    working speed direction open loop

--- MotorControl/BasicSpeedDirectionControl/BasicSpeedDirectionControl.ino
+++ MotorControl/BasicSpeedDirectionControl/BasicSpeedDirectionControl.ino
@@ -17,17 +17,23 @@ void loop(){
   
 val = analogRead(analogInPin); 
 val2 = val-512;
-mag = abs(val2*(255/512));
+mag = abs(val2/2);
+if(mag>255){
+  mag = 255;
+}
+else{
+  mag = mag;
+}
   
  if(val2<0){
-   analogWrite(analogOutPin2,0);
+   digitalWrite(analogOutPin2,LOW);
    analogWrite(analogOutPin1,mag);
-   analogWrite(analogOutPin3,255);
+   digitalWrite(analogOutPin3,HIGH);
  }
  else{
-   analogWrite(analogOutPin3,0);
+   digitalWrite(analogOutPin3,LOW);
    analogWrite(analogOutPin1,mag);
-   analogWrite(analogOutPin2,255);
+   digitalWrite(analogOutPin2,HIGH);
  }
  Serial.println(val);
  Serial.println(mag);
