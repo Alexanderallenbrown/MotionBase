d9ea1da89dde4479f6b29de80d02393f2cd913c0
megantuttle <tuttlem21@gmail.com>
2016-05-04 11:07:01 -0400

    arduino code changes

--- MotorControl/encoder_P_Position_potonly/encoder_P_Position_potonly.ino
+++ MotorControl/encoder_P_Position_potonly/encoder_P_Position_potonly.ino
@@ -10,16 +10,17 @@ const int analogOutPin1=11;
 const int analogOutPin2=9;
 const int analogOutPin3=10;
 
-float k_pot = (390-100)/(2*PI);
+float k_pot = (320.0)/(PI/2);
 
 int val = 0; 
 int val2 = 0;
 int mag = 0;
 
-static int globzeros[6] = {100,414,500,499,448,457};
+static int globzeros[6] = {140,414,500,499,448,457};
+static float motorsigns[6] = {1.0,1.0,1.0,-1.0,1.0,-1.0};
 
 //put the motor number for this arduino here:
-int motornum = 1;
+int motornum = 6;
 
 int glob0 = globzeros[motornum-1];
 
@@ -27,8 +28,8 @@ void setup()
 {
   Serial.begin(115200);
   //attach the interrupts
-  attachInterrupt(0, channelA,CHANGE);
-  attachInterrupt(1, channelB,CHANGE);
+  //attachInterrupt(0, channelA,CHANGE);
+  //attachInterrupt(1, channelB,CHANGE);
   pinMode(analogOutPin2,OUTPUT);
   pinMode(analogOutPin3,OUTPUT);
 //  int tot = 0;
@@ -41,7 +42,7 @@ void setup()
   Serial.print(globV);
   Serial.print('\t');
   int glob0 = 100;
-  float k_pot = (390-100)/(2*PI);
+  //float k_pot = (390.0-100.0)/(PI/4);
   float globPos = (globV - glob0)/k_pot;
   Serial.print(globPos);
   Serial.print('\t');
@@ -56,16 +57,16 @@ void loop()
   // create local variables to hold a local copies of the channel inputs
   // these are declared static so that thier values will be retained
   // between calls to loop.
-  static long unCount;
+  //static long unCount;
         	
-  noInterrupts(); // turn interrupts off quickly while we take local copies of the shared variables
+  //noInterrupts(); // turn interrupts off quickly while we take local copies of the shared variables
 
-  unCount = unCountShared;
+  //unCount = unCountShared;
  
   //interrupts(); 
-  
+  int rawpos = analogRead(1);
   //float posfloat = unCount*2*PI/(8000.0); //our encoder position in radians
-  float posfloat = (analogRead(1) - glob0)/k_pot;
+  float posfloat = motorsigns[motornum-1]*(rawpos - glob0)/k_pot;
   //val is desired angle
   //0-1023 = 0-pi
   val = analogRead(analogInPin); //raw reference position value.
@@ -73,9 +74,16 @@ void loop()
   
   float ref_command_float = val*PI/512.0;//this should be the reference position in radians.
   
+  if (ref_command_float>PI/2){
+    ref_command_float = PI/2;
+  }
+  if (ref_command_float<0){
+    ref_command_float = 0;
+  }
+  
   float float_error = ref_command_float-posfloat;//this is our current error value!!
   
-  float kp = 100.0/(PI/4.0);//some guess for KP.........
+  float kp = 255.0/(PI/6.0);//some guess for KP.........
   
   float float_U = kp*float_error;
   
@@ -108,11 +116,11 @@ void loop()
    digitalWrite(analogOutPin2,LOW);
  }
  
-  Serial.print(analogRead(1));
+  Serial.print(posfloat);
   Serial.print("\t");
   Serial.print(ref_command_float);
   Serial.print("\t");
-  Serial.print(unCount);
+  Serial.print(rawpos);
   Serial.print("\t");
   Serial.print(float_error);
   Serial.print("\t");
